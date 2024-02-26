2447416a1c134dac63de9c665615a33569679eb8
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-02-26 22:08:41 +0530

    java program to print date month year

---
+++ DateTimeYear.java
@@ -0,0 +1,14 @@
+// Java Program to Display Dates in Different Format
+
+import java.io.*;
+import java.time.*;
+
+class DateTimeYear {
+
+	public static void main(String[] args)
+	{
+		LocalDate date = LocalDate.now();
+
+		System.out.println(date);
+	}
+}
