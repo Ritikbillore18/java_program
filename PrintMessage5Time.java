fd8c318daa907a4202fb28e63649939e2de1b354
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-03-22 23:16:50 +0530

    Print a message in five time

---
+++ PrintMessage5Time.java
@@ -0,0 +1,12 @@
+//Write a program to print Hello and then print your name five times
+
+class PrintMessage5Time {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Hello");
+      for (int j = 0; j < 5; j++) {
+        System.out.println("Sterling Institute");
+      }
+    }
+  }
+}
