f1815e1cc9906197e483a93cdf6f63d8c2d6b0cc
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-03-29 23:39:11 +0530

    All Arthmetic Operators

---
+++ ArthmeticOperation.java
@@ -0,0 +1,12 @@
+// Arthmetic Operation
+public class ArthmeticOperator {
+ public static void main(String[] args){
+    int x=100;
+    int y=20;
+	System.out.println("Addition operator performed : x + y"+ (x+b));
+	System.out.println("Subtraction operator performed : x - y"+ (x-b));
+   	System.out.println("Multiplication operator performed : x * y"+ (x*b));
+	System.out.println("Division operator performed : x / y"+ (x/b));
+	System.out.println("Modulus operator performed : x % y"+ (x%b));
+ }
+}
