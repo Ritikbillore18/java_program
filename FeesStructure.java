a7e8267d1ef69f63a6296ec2cd0bb2048111e33b
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-03-31 22:20:58 +0530

    Add files via upload

---
+++ FeesStructure.java
@@ -0,0 +1,21 @@
+class FeesStructure
+{
+    public static void main(String[] args)
+    {
+        int paise = 1000;
+        {
+            if(paise <=5000)
+            {
+                System.out.println("This Couse is for Java ");
+            }
+            else if(paise == 3600)
+            {
+                System.out.println("this course is for only c and c ++ version ");
+            }
+            else
+            {
+                System.out.println("course for placement Batch");
+            }
+        }
+    }
+}
