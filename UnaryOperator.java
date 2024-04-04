205adb065c16a4d82a345059904d02e1f8e23ceb
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-04-04 20:56:10 +0530

    Add files via upload

---
+++ UnaryOperator.java
@@ -0,0 +1,21 @@
+// Unary Operator
+public class UnaryOperator{
+ public static void main(String[] args){
+   
+ int a=200;
+ int b=90;
+
+//- minus opertor
+  int ab = -a;
+  System.out.println("Unary minus operator : " +ab);
+
+//++ increment 
+  ++a;
+ System.out.println("Increment Operator : "+ a);
+
+//--decrement
+  --b;
+   System.out.println("decrement Operator : "+ b);
+ 
+ } 
+}
