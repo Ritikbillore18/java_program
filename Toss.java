98422ddd3ea257a27d48ac1e8e7a8d6bee7463ce
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-04-05 22:51:50 +0530

    Add files via upload

---
+++ Toss.java
@@ -0,0 +1,18 @@
+class Toss
+{
+	public static void main(String[] args)
+	{
+       	  	String a="Poorv",b="Pratik";
+		int c=5; 
+		System.out.println(a+" and "+b+" Play cricket for batting first they toss");
+		if (c==5)
+		{
+		   System.out.println(a+" Batting first");
+		}
+		else
+		{
+		   System.out.println(b+" Batting first");
+		}
+	}
+
+}
