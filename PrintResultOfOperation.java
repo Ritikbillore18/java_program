020d1928d330118922cb56237f5796ce7545f21f
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-04-17 20:21:17 +0530

    Add files via upload

---
+++ PrintResultOfOperation.java
@@ -0,0 +1,16 @@
+/* 4. Write a Java program to print the result of the following operations.
+Test Data:
+i. -5 + 8 * 6
+ii. (55+9) % 9
+iii. 20 + -3*5 / 8
+iv. 5 + 15 / 3 * 2 - 8 % 3   */
+
+class PrintResultOfOperation {
+	public static void main(String[] args){
+		System.out.println(-5 + 8 * 6);
+		System.out.println((55+9)%9);
+		System.out.println(20+(-3*5)/8);
+		System.out.println(5+15/3*2-8%3);
+
+	}
+}
