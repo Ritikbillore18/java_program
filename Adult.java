9126f5c4430bbf1d9b9221debabeca89e1f0866c
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-05-04 23:39:08 +0530

    Add files via upload

---
+++ Adult.java
@@ -0,0 +1,17 @@
+class Toss
+{
+	public static void main(String[] args)
+	{
+       	  	String a="Poorv",b="Pratik";
+		int c=6;
+		if (a=6)
+		{
+		   System.out.println(a+"Batting first");
+		}
+		else
+		{
+		   System.out.println(b+"Batting first");
+		}
+	}
+
+}
