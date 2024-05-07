84bc5b4893516025bed0a5f859227bcdefc484b5
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-05-07 14:41:55 +0530

    Add files via upload

---
+++ PrintStar.java
@@ -0,0 +1,16 @@
+// 6. Write a program to print the following pattern: /
+/*   	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *
+	* * * * *     */
+public class PrintStar{
+public static void main(String[] args){
+ for(int i=0; i<5 ;i++){
+  for (int j=0; j<5; j++){
+	System.out.print("* ");
+  }  
+    System.out.println(" ");
+ }
+}
+}
