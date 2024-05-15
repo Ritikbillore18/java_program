bf0b32e81e4d10444d2a57ea5da2eb97574feaf1
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-05-15 23:01:55 +0530

    Add files via upload

---
+++ ForloopPracctice.java
@@ -0,0 +1,11 @@
+public class ForloopPracctice{
+    public static void main(String[] args)
+    {
+         System.out.print("outside for loop \n");
+         System.out.print("Hellon \r");
+         System.out.print("Jaduuu  \t teri nazar");
+         System.out.print("Ritik Billlore \b");
+         System.out.print(" \ 'Ritik Billlore\ '  ");
+
+    }
+}
