50bf874af6f6c256cd6a6899799bb08505ddf34d
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-05-16 20:55:12 +0530

    Add files via upload

---
+++ LargestAmongThree.java
@@ -0,0 +1,22 @@
+class LargestAmongThree{
+   public static void main(String[] args){
+   int a =10;
+   int b= 60;
+   int c=90;
+
+   if(a>b){
+         if(a<c){
+       System.out.println("a is greathest");
+	 } 
+         else{
+       System.out.println("c is greathest");
+	 } 
+     }
+  else if(b>c){
+    System.out.println("b is greathest");
+  }
+  else {
+    System.out.println("c is greathest"); 
+}
+ }
+}
