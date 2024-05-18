9a695973acca31d96facb9eed28df9bee2cbde30
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-05-18 22:48:04 +0530

    Add files via upload

---
+++ TablePrint.java
@@ -0,0 +1,11 @@
+// 4. Write a program to print table of 2
+
+public class TablePrint {
+public static void main (String[] args){
+    int a=5;
+    for (int i=1;i<=10;i++){
+      int t = a*i;
+      System.out.println(a + "*" + i + "=" + t);
+  }
+ }
+}
